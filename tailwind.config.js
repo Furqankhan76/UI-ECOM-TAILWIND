4466a070f7d5e0a8d0a7da97f8ea174c20f9172a
furqan <furqankhanfk0999@gmail.com>
2024-11-02 16:20:18 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'background-bar': "url('background bar.png')",
+      //   'footer-texture': "url('/img/footer-texture.png')",
+      // }
+    },
+  },
+  plugins: [],
+}
+
