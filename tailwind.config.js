1313dfe757b284325e4bde27ad27ddb39771a8ee
furqan <furqankhanfk0999@gmail.com>
2024-11-02 16:25:32 +0530

    update file structure for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./*.html"],
   theme: {
     extend: {
       // backgroundImage: {
@@ -10,5 +10,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
-
+};
